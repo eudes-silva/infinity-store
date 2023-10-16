dbef83c488f78769f6da4d59e429c203ba9fd10b
eudes-silva <eudes-silva@tutanota.com>
2023-10-16 01:27:47 -0300

    feat: 404 page not found setup

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <v-btn to="/">voltar para a home</v-btn>
+  <div class="d-flex align-center justify-center h-screen">
+    <div class="text-center">
+      <v-icon size="50" color="warning">mdi-alert-outline</v-icon>
+      <h2>404</h2>
+      <v-alert type="warning" variant="outlined"
+        >Página não encontrada.</v-alert
+      >
+      <v-btn class="mt-4" to="/" variant="outlined">voltar para a home</v-btn>
+    </div>
   </div>
 </template>
